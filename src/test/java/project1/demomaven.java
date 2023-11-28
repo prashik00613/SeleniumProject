6d9ca78be2733122d349ab1f1c274ea902de8491
Prashik <Prashik06@gmail.com>
2023-11-28 11:33:46 +0530

    Hiiii

---
+++ src/test/java/project1/demomaven.java
@@ -0,0 +1,13 @@
+package project1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class demomaven {
+
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+		System.out.println("hello");
+	}
+
+}
